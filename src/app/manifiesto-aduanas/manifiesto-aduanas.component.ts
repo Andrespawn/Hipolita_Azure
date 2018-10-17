c2d66cdfd62f5eedf740248abeeb87bf5b1acd94
Daniel <dsanchez@assertsolutions.com>
2018-10-17 10:11:10 -0500

    Manifiesto Aduanas
    Desarrollo del MockUp Manifiesto Aduanas

--- src/app/manifiesto-aduanas/manifiesto-aduanas.component.ts
+++ src/app/manifiesto-aduanas/manifiesto-aduanas.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManifiestoAduanasComponent implements OnInit {
 
+msjValidacion:string = "";
+mostarMsjValidacion:boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  generarDocAduanas(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const nroGuia: string = target.querySelector('#txtNroGuia').value;
+    
+if(nroGuia == ""){
+  this.msjValidacion = "Debe diligenciar el campo 'Número Guía Master'";
+  this.mostarMsjValidacion = true;
+}else{
+  this.msjValidacion = "";
+  this.mostarMsjValidacion = false;
+}
+
   }
 
 }
